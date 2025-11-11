a49672115c5bf0791245e7a5be48f42d36d38c9a
khadijakhiboua12 <khadijakhiboua12@gmail.com>
2025-11-11 12:11:02 +0100

    Afficher data concernet le  select  de destination

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ "use strict";
 document.addEventListener("DOMContentLoaded", () => {
     const emailInput = document.getElementById("email");
     const passwordInput = document.getElementById("password");
@@ -24,7 +25,8 @@ document.addEventListener("DOMContentLoaded", () => {
             if (userFound) {
                 alert("Connexion réussie !");
                 localStorage.setItem("loggedUser", JSON.stringify(userFound));
-                window.location.href = "index.html";
+                // window.location.href = "index.html";
+                window.location.href= "destinations.html";
             } else {
                 alert("Email ou mot de passe incorrect !");
             }
@@ -33,45 +35,63 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 ;
 //L'AFFICHAGE D'UNE BUTTON LOGOUT  UNE FOIS FAIT LOGIN IN
-const userInfo=document.getElementById("userInfo");
-const logoutBtn=document.getElementById("logoutBtn");
+const userInfo = document.getElementById("userInfo");
+const logoutBtn = document.getElementById("logoutBtn");
 
-const loggedUser=JSON.parse(localStorage.getItem('loggedUser'));
+const loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
 
-if(userInfo && logoutBtn){
-if(loggedUser){
- userInfo.textContent=`Logged in  as : ${loggedUser.email}`;
- logoutBtn.style.display='inline-block';
-}else{
- userInfo.textContent='Not logged in';
- logoutBtn.style.display='none';
-}
+if (userInfo && logoutBtn) {
+    if (loggedUser) {
+        userInfo.textContent = `Logged in  as : ${loggedUser.email}`;
+        logoutBtn.style.display = 'inline-block';
+    } else {
+        userInfo.textContent = 'Not logged in';
+        logoutBtn.style.display = 'none';
+    }
 
-//logout function
-logoutBtn.addEventListener('click',()=>{
-    localStorage.removeItem('loggedUser');
-    userInfo.textContent='Not logged in';
-    logoutBtn.style.display='none';
-    window.location.href = "login.html";
+    //logout function
+    logoutBtn.addEventListener('click', () => {
+        localStorage.removeItem('loggedUser');
+        userInfo.textContent = 'Not logged in';
+        logoutBtn.style.display = 'none';
+        window.location.href = "login.html";
 
-});
+    });
 }
 //Pour destination
 let accommodation_data = [];
-let destinations_data = [];
+
 let spacecraft_data = [];
 let booking_option_data = [];
 
-function loadData(){
-  fetch("destinations.json")
-    .then(res => res.json())
-    .then(data => {
-      localStorage.setItem("destination", JSON.stringify(data));
-      console.log(data.destinations); 
-    })
-    .catch(err => console.log("Erreur JSON:", err));
+let destinations_data = [];
+
+function loadData() {
+    fetch("destinations.json")
+        .then(res => res.json())
+        .then(data => {
+
+            localStorage.setItem("destinations", JSON.stringify(data.destinations));
+            destinations_data = data.destinations;
+            console.log(" Destinations chargées :", destinations_data);
+            Affiche_Destination_option();
+        })
+        .catch(err => console.log(" Erreur JSON:", err));
+}
+
+
+function Affiche_Destination_option() {
+    const booking_form = document.getElementById("booking-form");
+    if (booking_form) {
+        const selectDest = document.getElementById("SelectDestination");
+        // selectDest.innerHTML = 'Select your destination';
+        destinations_data.forEach(dest => {
+            const option = document.createElement("option");
+            option.textContent = dest.name;
+            option.value = dest.id;
+            selectDest.appendChild(option);
+        });
+    }
 }
 loadData();
-
-//Pour select
-const select=document.getElementById("SelectDestination");
+//Fonction  de  
