1d447af46a0c1f04bdb9acb57da05e7692636aa2
khadijakhiboua12 <khadijakhiboua12@gmail.com>
2025-11-11 09:14:13 +0100

    Recupere data a partir de fichier destination.json

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
-//Pour la page login
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const loginBtn = document.getElementById("loginBtn");
+document.addEventListener("DOMContentLoaded", () => {
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const loginBtn = document.getElementById("loginBtn");
 
-//Recuperer data a partir de localstorage
-const users = JSON.parse(localStorage.getItem("users")) || [];
+    const users = JSON.parse(localStorage.getItem("users")) || [];
 
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+    if (loginBtn) {
+        loginBtn.addEventListener("click", (e) => {
+            e.preventDefault();
 
-  if (!email || !password) {
-    alert("⚠️ Svp entrez votre email et mot de passe !");
-    return;
-  }
+            const email = emailInput.value.trim();
+            const password = passwordInput.value.trim();
 
-  // Vérifier si l'utilisateur existe
-  const userFound = users.find(
-    user => user.email === email && user.password === password
-  );
+            if (!email || !password) {
+                alert(" Svp entrez votre email et mot de passe !");
+                return;
+            }
 
-  if (userFound) {
-    alert("Connexion réussie !");
-    localStorage.setItem("loggedUser", JSON.stringify(userFound));
-    window.location.href = "index.html";
-  } else {
-    alert("Email ou mot de passe incorrect !");
-  }
+            const userFound = users.find(
+                user => user.email === email && user.password === password
+            );
+
+            if (userFound) {
+                alert("Connexion réussie !");
+                localStorage.setItem("loggedUser", JSON.stringify(userFound));
+                window.location.href = "index.html";
+            } else {
+                alert("Email ou mot de passe incorrect !");
+            }
+        });
+    }
 });
+;
 //L'AFFICHAGE D'UNE BUTTON LOGOUT  UNE FOIS FAIT LOGIN IN
 const userInfo=document.getElementById("userInfo");
 const logoutBtn=document.getElementById("logoutBtn");
@@ -37,7 +40,7 @@ const loggedUser=JSON.parse(localStorage.getItem('loggedUser'));
 
 if(userInfo && logoutBtn){
 if(loggedUser){
- userInfo.textContent=`Logged in  as:${loggedUser.email}`;
+ userInfo.textContent=`Logged in  as : ${loggedUser.email}`;
  logoutBtn.style.display='inline-block';
 }else{
  userInfo.textContent='Not logged in';
@@ -50,5 +53,25 @@ logoutBtn.addEventListener('click',()=>{
     userInfo.textContent='Not logged in';
     logoutBtn.style.display='none';
     window.location.href = "login.html";
+
 });
 }
+//Pour destination
+let accommodation_data = [];
+let destinations_data = [];
+let spacecraft_data = [];
+let booking_option_data = [];
+
+function loadData(){
+  fetch("destinations.json")
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem("destination", JSON.stringify(data));
+      console.log(data.destinations); 
+    })
+    .catch(err => console.log("Erreur JSON:", err));
+}
+loadData();
+
+//Pour select
+const select=document.getElementById("SelectDestination");
